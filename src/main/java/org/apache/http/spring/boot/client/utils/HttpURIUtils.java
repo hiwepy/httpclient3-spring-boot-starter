6320952811d0e656c466d2ad07fa957965efb032
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:34:35 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/apache/http/spring/boot/client/utils/HttpURIUtils.java
+++ src/main/java/org/apache/http/spring/boot/client/utils/HttpURIUtils.java
@@ -36,7 +36,7 @@ public abstract class HttpURIUtils {
 	/**
 	 * 
 	 * @description	：构建普通参数集合
-	 * @author [@Loong Wan](https://github.com/loong10k)
+	 * @author <a href="https://github.com/loong10k">Loong Wan</a>
 	 * @date 		：Jun 30, 2015 6:57:06 PM
 	 * @param baseURL
 	 * @param paramsMap
