4d76b4778f8327b85ad7e001bd3d055a57fca133
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:34:27 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/apache/http/spring/boot/client/utils/IOUtils.java
+++ src/main/java/org/apache/http/spring/boot/client/utils/IOUtils.java
@@ -22,7 +22,7 @@ import java.io.OutputStream;
 
 /**
  * TODO
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 public class IOUtils {
 
