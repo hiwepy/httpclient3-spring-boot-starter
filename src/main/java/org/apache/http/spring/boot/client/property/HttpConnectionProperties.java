772d10a388b73a850e64a35d76694da63e7ff5e0
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:34:07 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/apache/http/spring/boot/client/property/HttpConnectionProperties.java
+++ src/main/java/org/apache/http/spring/boot/client/property/HttpConnectionProperties.java
@@ -22,7 +22,7 @@ public class HttpConnectionProperties extends HttpConnectionParams {
 	/**
 	 * 
 	 * @description	： 处理默认参数
-	 * @author [@Loong Wan](https://github.com/loong10k)
+	 * @author <a href="https://github.com/loong10k">Loong Wan</a>
 	 * @date 		：2017年12月3日 下午9:13:14
 	 * @return
 	 */
